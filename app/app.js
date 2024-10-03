1a1b8c25c9975e464ec460e64b27470f8150a7da
jennawlin3 <jenniferacosta437@gmail.com>
2024-10-03 14:34:51 -0400

    updated index.html, styles.css and app.js

--- app/app.js
+++ app/app.js
@@ -8,6 +8,24 @@ const $desawBtn = d.querySelector(".desaw-btn");
 const $wordpressBtn = d.querySelector(".wordpress-btn");
 const $webDevCards = d.querySelector(".web-development-cards");
 const $wordpressCards = d.querySelector(".wordpress-cards");
+const desktopMediaQuery = window.matchMedia("(min-width: 1024px)");
+const otherMediaQuery = window.matchMedia("(max-width: 1023px)");
+
+desktopMediaQuery.addEventListener("change", (e) => {
+    console.log("hola");
+    console.log(e.matches);
+    if(e.matches) {
+        $overlay.classList.add("hide");
+    }
+});
+
+otherMediaQuery.addEventListener("change", (e) => {
+    if(e.matches) {
+    if($header.classList.contains("menu")) {
+        $overlay.classList.remove("hide");
+    }        
+    }
+})
 
 d.addEventListener("DOMContentLoaded", (e) => {
     if(window.scrollY >= 100) {
@@ -16,7 +34,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
 })
 
 window.addEventListener("scroll", (e) => {
-    console.log(window);
+    console.log("hola");
     if(window.scrollY >= 100) {
         $header.classList.add("active");
     } else {
@@ -26,12 +44,14 @@ window.addEventListener("scroll", (e) => {
 
 $menuIcon.addEventListener("click", (e) => {
     $mobileNav.classList.remove("hide");
+    $header.classList.add("menu");
     $overlay.classList.remove("hide");
 });
 
 $closeIcon.addEventListener("click", (e) => {
     $mobileNav.classList.add("hide");
     $overlay.classList.add("hide");
+    $header.classList.remove("menu");
 });
 
 $desawBtn.addEventListener("click", (e) => {
